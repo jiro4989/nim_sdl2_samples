11d433b1273bbac1beb12062a5c6919efaf086ff
jiro4989 <jiroron666@gmail.com>
2020-01-08 21:00:40 +0900

    add sample 3

--- src/sample_003_draw_image.nim
+++ src/sample_003_draw_image.nim
@@ -1,4 +1,4 @@
-import sdl2, sdl2/gfx
+import sdl2, sdl2/gfx, sdl2/image
 
 discard sdl2.init(INIT_EVERYTHING)
 
@@ -6,7 +6,7 @@ var
   window: WindowPtr
   render: RendererPtr
 
-window = createWindow("sample_001_window.nim", 100, 100, 640,480, SDL_WINDOW_SHOWN)
+window = createWindow("sample_003_draw_image.nim", 100, 100, 640,480, SDL_WINDOW_SHOWN)
 render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
 
 var
@@ -15,16 +15,34 @@ var
   fpsman: FpsManager
 fpsman.init
 
+# new code
+let texture = render.loadTexture("assets/sample_003_draw_image.png")
+
 while runGame:
   while pollEvent(evt):
     if evt.kind == QuitEvent:
       runGame = false
       break
 
-  let dt = fpsman.getFramerate() / 1000
-
   render.setDrawColor 0,0,0,255
   render.clear
+
+  # ========================================
+  # begin new code
+  # ========================================
+  var
+    src = rect(0, 0, 110, 22)
+    dst = rect(0, 0, 110, 22)
+  discard render.copyEx(texture,
+    src,
+    dst,
+    angle = 0.0,
+    center = nil,
+    flip = SDL_FLIP_NONE,
+    )
+  # ========================================
+  # end new code
+  # ========================================
 
   render.present
   fpsman.delay
